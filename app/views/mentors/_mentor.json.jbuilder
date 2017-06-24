76be1f7af03bcb5dde77cf2cf5f979c647187906
zephyllr <lalice72098@yahoo.com>
2017-06-24 14:51:12 -0400

    ruby on rails framework

---
+++ app/views/mentors/_mentor.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! mentor, :id, :first_name, :last_name, :email, :skills, :created_at, :updated_at
+json.url mentor_url(mentor, format: :json)
---
+++ test/system/mentors_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class MentorsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit mentors_url
+  #
+  #   assert_selector "h1", text: "Mentor"
+  # end
+end
